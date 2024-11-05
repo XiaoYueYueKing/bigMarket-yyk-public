934cf2f69d11d9b48af6285a2c6972d2d44fa619
yueyueking <945362994@qq.com>
2024-11-05 22:57:38 +0800

    feat:不是很懂决策树是怎么筛选出需要的逻辑的，代码中暂时没看出来

---
+++ big-market-domain/src/main/java/cn/bugstack/domain/strategy/model/valobj/RuleTreeNodeVO.java
@@ -0,0 +1,34 @@
+package cn.bugstack.domain.strategy.model.valobj;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 规则树节点对象
+ * 决策树的节点，这些节点可以组合出任意需要的规则树
+ * @create 2024-01-27 10:48
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RuleTreeNodeVO {
+
+    /** 规则树ID */
+    private Integer treeId;
+    /** 规则Key */
+    private String ruleKey;
+    /** 规则描述 */
+    private String ruleDesc;
+    /** 规则比值 */
+    private String ruleValue;
+
+    /** 规则连线 */
+    private List<RuleTreeNodeLineVO> treeNodeLineVOList;
+
+}
