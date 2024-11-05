88efd8340b3e498d0f374ecc2ca61a17f5e893aa
yueyueking <945362994@qq.com>
2024-11-05 21:43:49 +0800

    feat:试试代码评审模块有没有用

--- big-market-app/src/main/java/cn/bugstack/Application.java
+++ big-market-app/src/main/java/cn/bugstack/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Configurable
 public class Application {
     public static void main(String[] args){
+
         SpringApplication.run(Application.class);
     }
 }
